76c6250e149f83c3cc2604702703199eb09f4c92
Rashidul Hasan <rhratul12@gmail.com>
2022-08-11 02:59:45 +0600

    Parse XLSX node event  add

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,28 @@
 const { defineConfig } = require("cypress");
+const xlsx = require("node-xlsx");
+const fs = require("fs");
+const path = require("path");
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      // `on` is used to hook into various events Cypress emits
+      on("task", {
+        parseXlsx( filePath ) {
+          return new Promise((resolve, reject) => {
+            try {
+              const jsonData = xlsx.parse(fs.readFileSync(filePath));
+              resolve(jsonData);
+            } catch (e) {
+              reject(e);
+            }
+          });
+        },
+      });
     },
     baseUrl: "https://demoqa.com",
     watchForFileChanges: false,
-     chromeWebSecurity: false,
+    chromeWebSecurity: false,
   },
 });
